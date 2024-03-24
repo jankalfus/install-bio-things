2ecb5d8ecc9f0488e9713837d50cd8a54b6c1211
Jan Kalfus <jankalfus@gmail.com>
2024-03-24 19:18:54 +0100

    Add script for running stainedglass

---
+++ run-stained-glass.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd $HOME/StainedGlass/
+eval "$(conda shell.bash hook)"
+conda activate stainedglass
+for file in \
+  $HOME/StainedGlass/data/Rca2_R5.fa 
+do
+  samtools faidx "$file"
+  filename=$(basename "$file")
+  snakemake --use-conda --cores all --config sample="${filename%.*}" fasta="$file"
+  snakemake --use-conda --cores all make_figures --config sample="${filename%.*}" fasta="$file"
+done
